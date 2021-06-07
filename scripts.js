230ad818bde6494b37af39d637be4a8918d493cd
Jerry Zhang <jerryzhang101@yahoo.com>
2021-06-07 14:50:00 -0400

    Added functions to input values into and clear display

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Basic 4 functions of calculator
 function add(a, b) {
     return a + b;
 }
@@ -14,6 +15,37 @@ function divide(a, b) {
     return a / b; //This function will need some tweeking later (too many decimals)
 }
 
+//Function to actually calculate something
 function operate(a, b, func) {
     return func(a, b);
+}
+
+//Places eventListener on all digits
+const digitDiv = document.querySelector("#digitDiv");
+const childrenNodes = digitDiv.childNodes;
+const listLength = childrenNodes.length;
+for (let i = 1; i < listLength/2; i++) {
+    childrenNodes[i*2 - 1].addEventListener("click", function(e) {
+        if (e.target.id == "c") {
+            clear()
+        }
+        else{
+            console.log(e.target.id);
+            let input = e.target.id;
+            enterValue(input);
+        }
+    });
+}
+
+//Function to input number values into calculator
+function enterValue(value) {
+    let display = document.querySelector("#output");
+    display.textContent = display.textContent + value;
+    
+}
+
+//Function to clear the display
+function clear() {
+    let display = document.querySelector("#output");
+    display.textContent = "";
 }
